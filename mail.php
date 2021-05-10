b3ea166965de5c59e7d22a739503ce01f5ad0289
Adnan-Bacic <adnan-bacic@hotmail.com>
2021-05-10 16:04:58 +0200

    checking for empty values in mail

--- mail.php
+++ mail.php
@@ -11,38 +11,36 @@ error_reporting(E_ALL);
 */
 			
 if(isset($_POST['submit'])){
-    $email = $_POST['email'];
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-        
+    if(isset($_POST['email'], $_POST['name'], $_POST['phone'], $_POST['subject'], $_POST['message'])){
+        //empty strings count as isset() so make sure there is data
+        if(!empty($_POST['email']) && !empty($_POST['name']) && !empty($_POST['phone']) && !empty($_POST['subject']) && !empty($_POST['message'])){
+            
+            $email = $_POST['email'];
+            $name = $_POST['name'];
+            $phone = $_POST['phone'];
+            $subject = $_POST['subject'];
+            $message = $_POST['message'];
+            
+            $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+                
 
-    $valid = 0;
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $valid = 1;
-    } else {
-    $valid = 0;
-    }
+            $valid = 0;
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $valid = 1;
+            }
 
-    $emailTo = 'kontakt@nordiskmusculupati.dk';
-    $headers = 'From: ' . $email;
-    $inboxText = "Name: {$name} \n E-mail: {$email} \n Phone: {$phone} \n Subject: {$subject} \n Message: {$message}";
+            $emailTo = 'kontakt@nordiskmusculupati.dk';
+            $headers = "From: {$email}";
+            $inboxText = "Name: {$name} \n E-mail: {$email} \n Phone: {$phone} \n Subject: {$subject} \n Message: {$message}";
 
-    $mes = wordwrap($message, 70);
+            $mes = wordwrap($message, 70);
 
-    $res = 0;
-    if($valid == 1){
-    //send mail
-    $res = mail($emailTo, $subject, $inboxText, $headers);
-    }
-
-    if($res == 1){
-        $_SESSION["msg"] = "Email er sendt";
-    } else {
-        $_SESSION["msg"] = "Email ikke sendt";
+            $res = 0;
+            if($valid == 1){
+                //send mail
+                $res = mail($emailTo, $subject, $inboxText, $headers);
+            }
+        }
     }
 }
 ?>
@@ -55,21 +53,21 @@ if(isset($_POST['submit'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nordisk Musculupati</title>
     <meta name="robots" content="noindex" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <?php
     if($res == 1){
         ?>
         <script>
             localStorage.setItem('mailSent', '1');
-            window.history.back();
-            //window.location.href = "http://localhost:3000/kontakt";
+            //window.history.back();
+            window.location.href = "https://nordiskmusculupati.dk/kontakt";
         </script>
         <?php
     } else {
         ?>
         <script>
             localStorage.setItem('mailSent', '0');
-            window.history.back();
+            //window.history.back();
+            window.location.href = "https://nordiskmusculupati.dk/kontakt";
         </script>
         <?php
     }
